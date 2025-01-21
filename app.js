ae2bec53d1f2d08511223e81ff34a5b65aacaffc
luvalenciaq <luisamariavalenciaq@gmail.com>
2025-01-21 18:41:29 -0500

    add: nuevas funcionalidades(actualizar, sortear)

--- app.js
+++ app.js
@@ -8,12 +8,39 @@ function agregarAmigo(){
         alert('Por favor, inserte un nombre');
     } else {
         listaAmigos.push(amigo);
-        console.log(listaAmigos);
         limpiarCaja();
+        actualizarLista(listaAmigos);
     }
 }
 
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
+}
+
+function actualizarLista(listaAmigos){
+    let listaActualizada = document.getElementById('listaAmigos');
+    listaActualizada.innerHTML = '';
+
+    for(let i = 0; i < listaAmigos.length; i++){
+        const li = document.createElement('li');
+        li.textContent = listaAmigos[i];
+        listaActualizada.appendChild(li);
+    }
+}
+
+function sortearAmigo(lista){
+    //validar que el array no esté vacío
+    if(lista.length == 0){
+       alert('Debe ingresar el nombre de sus amigos');
+       return;
+    }
+    //generar el aleatorio
+    let aleatorio = Math.floor(Math.random()*lista.length);
+
+    //obtener el nombre sorteado y mostrarlo
+    let amigoSorteado = lista[aleatorio];
+    document.getElementById("resultado").innerHTML = "Amigo sorteado: " + amigoSorteado;
+    
+    
 }
 
